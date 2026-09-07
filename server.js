02d41aa122607c44a2f23981e15a9e01cbff2ffc
Brayan Ramos <bryanjosueeesz@gmail.com>
2026-09-07 13:19:35 -0500

    Fix root route

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "100kb" }));
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 function escapeHtml(value) {
     if (value === null || value === undefined) {
